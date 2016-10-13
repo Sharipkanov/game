a61b0f52b39272de8806564bceaa067609fd9851
sharipkanov <sharipkanov@gmail.com>
2016-10-13 17:57:25 +0600

    index page content

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,6 +1,17 @@
 $(document).ready(function () {
     var forms = $('.uk-form');
 
+    var scrollSettings = getBrowserScrollSize();
+
+    $('.uk-modal').on({
+        'show.uk.modal': function(){
+            $('.main-header-cont').css('padding-right', scrollSettings.width);
+        },
+        'hide.uk.modal': function(){
+            $('.main-header-cont').css('padding-right', 0);
+        }
+    });
+
     $('.phone-input').mask('+7 (000) 000-0000');
 
     forms.submit(function (e) {
@@ -57,4 +68,32 @@ function clearForm(formData) {
             }
         }
     }
+}
+
+function getBrowserScrollSize(){
+    var css = {
+        "border":  "none",
+        "height":  "200px",
+        "margin":  "0",
+        "padding": "0",
+        "width":   "200px"
+    };
+
+    var inner = $("<div>").css($.extend({}, css));
+    var outer = $("<div>").css($.extend({
+        "left":       "-1000px",
+        "overflow":   "scroll",
+        "position":   "absolute",
+        "top":        "-1000px"
+    }, css)).append(inner).appendTo("body")
+        .scrollLeft(1000)
+        .scrollTop(1000);
+
+    var scrollSize = {
+        "height": (outer.offset().top - inner.offset().top) || 0,
+        "width": (outer.offset().left - inner.offset().left) || 0
+    };
+
+    outer.remove();
+    return scrollSize;
 }
