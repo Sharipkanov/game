5205927a36a636c9979edd9b624e72ca41422ab5
sharipkanov <sharipkanov@gmail.com>
2016-10-13 17:33:28 +0600

    index page content

--- app/js/custom.js
+++ app/js/custom.js
@@ -1,10 +1,60 @@
 $(document).ready(function () {
+    var forms = $('.uk-form');
+
     $('.phone-input').mask('+7 (000) 000-0000');
 
-    $('[data-uk-slideset]').on('show.uk.slideset', function(e){
-        var sliderContainer = $(this).find('.uk-slideset'),
-            slideHeight = sliderContainer.find('li.uk-active').outerHeight();
+    forms.submit(function (e) {
+        e.preventDefault();
 
-        sliderContainer.css('height', slideHeight);
+        var form = e.target,
+            formData = serializeForm(form);
+
+        // TODO form upload
+
+        setTimeout(function () {
+            clearForm(form)
+        }, 2000);
+
+        return false;
+    });
+
+    forms.each(function (e) {
+        clearForm(this);
     });
 });
+
+
+function serializeForm(formData) {
+    var returnData = {};
+    for(var i=0; i<formData.length; i++) {
+        var currentData = formData[i],
+            value = currentData.value,
+            name = currentData.name;
+
+        if(value) {
+            if(currentData.type === 'radio' || currentData.type === 'checkbox'){
+                if(currentData.checked === true) returnData[name] = value;
+            } else {
+                returnData[name] = value;
+            }
+        }
+    }
+
+    return returnData;
+}
+
+function clearForm(formData) {
+    for(var i=0; i<formData.length; i++) {
+        var currentData = formData[i],
+            value = currentData.value,
+            name = currentData.name;
+
+        if(value) {
+            if(currentData.type === 'radio' || currentData.type === 'checkbox'){
+                if(currentData.checked === true) currentData.checked = false;
+            } else {
+                currentData.value = "";
+            }
+        }
+    }
+}
